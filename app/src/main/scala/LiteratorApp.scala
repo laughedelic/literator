9ede071a0438fef007ff05f397dddc86bb3eed5b
Alexey Alekhin <laughedelic@gmail.com>
2013-11-30 05:57:06 +0100

    Added real options and docs generating code (as in plugin)

--- app/src/main/scala/LiteratorApp.scala
+++ app/src/main/scala/LiteratorApp.scala
@@ -1,13 +1,4 @@
-/*
-### Command line interface
-
-* Input: 
-  + a directory with sources or a single file
-  + (optional) destination: path prefix or a file name respectively
-* Output:
-  + if second argument is given, file(s) with documentation
-  + if not, it's set to `docs/` or `stdout` respectively
-*/
+/* ### Command line application */
 
 package laughedelic.literator.app
 
@@ -19,40 +10,59 @@ import org.rogach.scallop._
 import buildinfo._
 
 
-// this is your conf class, see the Scallop docs for how to use this
+/* This is configuration class, defining command line options using Scallop */
 case class AppConf(arguments: Seq[String]) extends ScallopConf(arguments) {
 
-  version(s"literator ${BuildInfo.version} - generating docs from sources \n(c) 2013 Alexey Alekhin (laughedelic)\n")
+  version(s"""|literator ${BuildInfo.version} - generating docs from sources
+              |(c) 2013 Alexey Alekhin (laughedelic)
+              |""".stripMargin)
 
+  /*  Using this option user can set the mapping between source input dirs and 
+      docs output dirs. For example,
+      `-M app/src=docs/src/app lib/src=docs/src/app`
+      becomes
+      `Map("app/src" -> "docs/src/app", "lib/src" -> "docs/src/app")`
+  */
   val docsMap = props[String](
-        descr = "Map between sources and docs output directories"
-      , keyName = "input-source"
-      , valueName = "output-docs"
+        name = 'M'
+      , descr = "Map between sources and docs directories"
+      , keyName = "input"
+      , valueName = "output"
       )
+  // TODO: validate existence of all files (+maybe warn if output dirs are not empty)
 }
 
 
-// this is the class that the sbt-launcher will use
+/* This is the class that the sbt-launcher will use */
 class Main extends xsbti.AppMain {
   def run(config: xsbti.AppConfiguration) = new Exit(Main.exec(config.arguments))
 }
 
 case class Exit(val code: Int) extends xsbti.Exit
 
+
+/* This object contains the actual code for running */
 object Main {
-  // this is the actual code for running
   def exec(args: Array[String]): Int = {
-    // TODO: use parsed opts
+
     val conf = AppConf(args)
 
-    // if (args.length < 1) sys.error("Need at least one argument")
-    // else file(args(0)).literate(if (args.length > 1) Some(file(args(1))) else None)
+    if (conf.docsMap.isEmpty) conf.printHelp()
 
-    // success!
+    /* Here we just iterate over the map, generate docs and output errors if any */
+    conf.docsMap foreach { case (in, out) =>
+      println("Generating documentation for " + in)
+
+      val errors = file(in).literate(Some(file(out)))
+      errors foreach { println }
+
+      if (errors.nonEmpty) sys.error("Couldn't generate documentation due to parsing errors")
+      else println("Documentation is written to  " + out)
+    }
+
     0
   }
 
-  // for testing within sbt
-  // 'run <args>' will execute your cmdline app
+  /* For testing within sbt 'run <args>' will execute this app */
   def main(args: Array[String]): Unit = exec(args)
 }
